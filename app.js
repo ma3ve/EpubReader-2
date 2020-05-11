672bebe0571da617ee25459553123d8935f16899
maeve <mujammilkhan321@gmail.com>
2020-05-11 23:17:56 +0530

    asd

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(require("./routes/uploadRoute"));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
     app.use((req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        res.sendFile(path.join(__dirname, "client/build/index.html"));
     });
 }
 
