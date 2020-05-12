da1fecd3c8cc515261747327f0bb5034c971dd91
maeve <mujammilkhan321@gmail.com>
2020-05-12 05:48:06 +0530

    typos

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`app is listening on port ${PORT}`);
